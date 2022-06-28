bd0c1b7910d68b0d206c6649423c3fea7c1f907b
Grefidy <grebennik2000@yandex.ru>
2022-06-28 18:18:01 +0300

    Добавлена задача 2.1

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,18 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button @click="$emit('update:count', count + 1)" type="button">{{ count }}</button>`,
 });
