c1ae7fc0375bc78673cdf1b90aba1f951cd9ae9a
Grefidy <grebennik2000@yandex.ru>
2022-08-09 16:27:36 +0300

    Добавлена задача 10.2

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,7 @@
 <template>
-  <!-- -->
+  <slot v-if="!isResolved" name="pending"></slot>
+  <slot v-if="isFulfilled" :result="result" name="fulfilled"></slot>
+  <slot v-if="isRejected" :error="error" name="rejected"></slot>
 </template>
 
 <script>
@@ -12,5 +14,42 @@ export default {
       required: true,
     },
   },
+
+  data() {
+    return {
+      isResolved: false,
+      isFulfilled: false,
+      isRejected: false,
+      result: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    promise: {
+      immediate: true,
+      handler(newPromise, oldPromise) {
+        this.isResolved = false;
+        this.isFulfilled = false;
+        this.isRejected = false;
+        this.error = null;
+        this.result = null;
+        newPromise
+          .then((result) => {
+            this.isResolved = true;
+            this.isFulfilled = true;
+            this.result = result;
+            return result;
+          })
+          .catch((error) => {
+            this.isResolved = true;
+            this.isRejected = true;
+            this.error = error;
+          });
+      },
+    },
+  },
+
+  computed: {},
 };
 </script>
