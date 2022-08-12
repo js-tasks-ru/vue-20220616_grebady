4285704d069734506e4f2e5f2d45aaffab120e72
Grefidy <grebennik2000@yandex.ru>
2022-08-12 15:25:27 +0300

    Подсмотрел решение преподавателя - понравилось

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,5 +1,12 @@
 import { createApp } from './vendor/vue.esm-browser.js';
 
+const calculatorOperators = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b === 0 ? 'На ноль делить не будем' : a / b),
+};
+
 // Создайте Vue приложение
 const app = createApp({
   data() {
@@ -17,17 +24,7 @@ const app = createApp({
   },
   computed: {
     result() {
-      switch (this.operator) {
-        case 'subtract':
-          return this.arg1 - this.arg2;
-        case 'multiply':
-          return this.arg1 * this.arg2;
-        case 'divide':
-          if (this.arg2 === 0) return 'На ноль делить не будем';
-          return this.arg1 / this.arg2;
-        default:
-          return this.arg1 + this.arg2;
-      }
+      return calculatorOperators[this.operator]?.(this.a, this.b);
     },
   },
 }).mount('#app');
