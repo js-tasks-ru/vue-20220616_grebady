53761bccac0ec4cccf41977046d67208799b1dd0
Grefidy <grebennik2000@yandex.ru>
2023-04-13 17:53:21 +0300

    ref(02/07-PageMeetup) Обновлен код проекта в соответствие с решением преподавателя

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -4,6 +4,13 @@ import UiAlert from './UiAlert.js';
 import { fetchMeetupById } from './meetupService.js';
 import MeetupView from '../06-MeetupView/MeetupView.js';
 
+const States = {
+  IDLE: 'IDLE',
+  LOADING: 'LOADING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+};
+
 export default defineComponent({
   name: 'PageMeetup',
 
@@ -12,6 +19,8 @@ export default defineComponent({
     UiContainer,
     MeetupView,
   },
+
+  States,
 
   props: {
     meetupId: {
@@ -22,15 +31,15 @@ export default defineComponent({
 
   data() {
     return {
+      state: States.IDLE,
       meetup: null,
       errorMessage: null,
     };
   },
 
   watch: {
-    meetupId(newId, oldId) {
-      this.meetup = null;
-      this.getMeetup(newId);
+    meetupId() {
+      this.getMeetup();
     },
   },
 
@@ -39,26 +48,33 @@ export default defineComponent({
   },
 
   methods: {
-    getMeetup(id) {
-      fetchMeetupById(id)
-        .then((meetup) => {
-          this.meetup = meetup;
-        })
-        .catch((e) => {
-          this.errorMessage = e.message ?? 'Ошибка при загрузки митапа';
-        });
+    async getMeetup() {
+      // Переводим состояние в "загрузка". Чистим данные и ошибку
+      this.state = States.LOADING;
+      this.meetup = null;
+      this.errorMessage = null;
+
+      try {
+        this.meetup = await fetchMeetupById(this.meetupId);
+        // Данные успешно загрузились
+        this.state = States.SUCCESS;
+      } catch (e) {
+        // Произошла ошибка при получении данных
+        this.errorMessage = e.message ?? 'Ошибка при загрузки митапа';
+        this.state = States.ERROR;
+      }
     },
   },
 
   template: `
     <div class="page-meetup">
-      <meetup-view v-if="meetup" :meetup="meetup"/>
+      <meetup-view v-if="state === $options.States.SUCCESS" :meetup="meetup"/>
 
-      <ui-container v-else-if="!errorMessage">
+      <ui-container v-else-if="state === $options.States.LOADING">
         <ui-alert >Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container v-else>
+      <ui-container v-else-if="state === $options.States.ERROR">
         <ui-alert >{{ errorMessage }}</ui-alert>
       </ui-container>
     </div>`,
